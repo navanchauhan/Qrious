056eec2d13ee7c0e6ea80c21e2b4bc8f2a4f445c
Navan Chauhan <navanchauhan@gmail.com>
2020-07-24 11:19:43 +0530

    Initial Qrious v0.1

---
+++ Qrious/ContentView.swift
@@ -0,0 +1,193 @@
+//
+//  ContentView.swift
+//  Qrious
+//
+//  Created by Navan Chauhan on 22/07/20.
+//  Copyright © 2020 Navan Chauhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct Todo: Codable, Identifiable {
+    public var id: Int
+    public var title: String
+    public var published_date: String
+    public var defined_type_name: String
+    public var url: String
+    public var description1: String?
+    public var paperURL: String?
+    public var candidates: String?
+    
+}
+
+
+// group=13668 ( Chemrxiv )
+class FetchToDo: ObservableObject {
+    @Published var query: String = ""
+    @Published var todos = [Todo]()
+     
+    init() {
+        //var request = URLRequest(url: URL(string: "https://api.figshare.com/v2/articles/search?search_for=\(q)&page_size=\(p)")!)
+        var request = URLRequest(url: URL(string: "https://api.figshare.com/v2/articles/search?page_size=20")!)
+        request.httpMethod = "POST"
+        
+        //let url = URL(string: "https://jsonplaceholder.typicode.com/todos")!
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            do {
+                if let todoData = data {
+                    let decodedData = try JSONDecoder().decode([Todo].self, from: todoData)
+                    DispatchQueue.main.async {
+                        self.todos = decodedData
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch {
+                print("Error")
+            }
+        }.resume()
+    }
+    func fetchAgain(q: String = "Hepatitis+B+Virus+Silico",p: String = "10",limit: Bool = false) {
+        var s = "https://api.figshare.com/v2/articles/search?search_for=\(q.replacingOccurrences(of: " ", with: "+"))&page_size=\(p)"
+        //var request = URLRequest(url: URL(string: "https://api.figshare.com/v2/articles/search?search_for=\(q)&page_size=\(p)")!)
+        if limit {
+            s += "&group=13668"
+        }
+        print(s, limit)
+        var request = URLRequest(url: URL(string: s)!)
+        request.httpMethod = "POST"
+        
+        //let url = URL(string: "https://jsonplaceholder.typicode.com/todos")!
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            do {
+                if let todoData = data {
+                    let decodedData = try JSONDecoder().decode([Todo].self, from: todoData)
+                    DispatchQueue.main.async {
+                        self.todos = decodedData
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch {
+                print("Error")
+            }
+        }.resume()
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+class configuration: ObservableObject {
+    @Published var question: String = "Results show what are the compounds?"
+}
+
+struct ContentView: View {
+    @State private var selection = 0
+    @State private var advance = true
+    @State public var query = "Covid-19 Compounds"
+    @State public var noOfArticles = "15"
+    @State private var executed = false
+    @EnvironmentObject var config: configuration
+    @State private var ques: String = configuration().question
+    //"Results show what top compounds?"
+    @State public var LimitToChemrxiv: Bool = false
+    @ObservedObject var fetch = FetchToDo()
+    //let bert = BERTQAFP16()
+    var body: some View {
+        TabView(selection: $selection){
+            NavigationView{
+                
+                VStack{
+                    /*
+                    HStack{
+                        
+                    Text("Configuration")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                    
+                    }*/
+                Form{
+                    
+                    Section(header: Text("Search Query")){
+                            TextField("Search Query",text: $query)
+                    }
+                    
+                    
+                    Section(header: Text("Configuration")){
+                        VStack{
+                        HStack{
+                            Text("No. Of Articles:")
+                            TextField("No. Of Articles", text: $noOfArticles)
+                        .keyboardType(.numberPad)
+                        }.disabled(!advance)
+                            HStack{
+                                Toggle(isOn: $LimitToChemrxiv){
+                                    Text("Limit to ChemRxiv")
+                                }
+                            }
+                        }
+                    }
+                        
+                        Section(header: Text("Query for AI Model")){
+                            TextField("",text: $ques, onEditingChanged: {_ in self.config.question = self.ques})
+                            
+                        }
+                        
+                    
+                    Section{
+                        Button(action: {self.fetch.fetchAgain(q: self.query, p: String(self.noOfArticles), limit: self.LimitToChemrxiv)}){
+                            Text("Get Papers")
+                        }
+                    }
+                    
+                }
+                    Text("OwO")
+                        .frame(height: 50.0)
+                }
+            .navigationBarTitle(Text("Qrious"))
+            }
+                .tabItem {
+                    VStack {
+                        Image(systemName: "gear")
+                        .font(.title) //Image("first")
+                        Text("Configuration")
+                    }
+                }
+                .tag(0)
+            NavigationView{
+            //Text(" View").font(.title)
+                VStack {
+                    List(fetch.todos) { todo in NavigationLink(destination:  DetailedView(article: todo)){
+                               VStack(alignment: .leading) {
+                                   Text(todo.title)
+                                   Text("\(todo.published_date)")
+                                       .font(.system(size: 11))
+                                       .foregroundColor(Color.gray)
+                            }}
+                           }
+                       }
+            .navigationBarTitle("Articles")
+            }
+                
+                .tabItem {
+                    VStack {
+                         Image(systemName: "list.bullet")
+                            .font(.title)
+                        Text("Articles")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Qrious/DetailedView.swift
@@ -0,0 +1,127 @@
+//
+//  DetailedView.swift
+//  Qrious
+//
+//  Created by Navan Chauhan on 22/07/20.
+//  Copyright © 2020 Navan Chauhan. All rights reserved.
+//
+
+
+import SwiftUI
+
+
+extension String {
+    public var withoutHtml: String {
+        guard let data = self.data(using: .utf8) else {
+            return self
+        }
+
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+
+        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
+            return self
+        }
+
+        return attributedString.string
+    }
+}
+
+struct DetailedView: View {
+    //public var description: String
+    //@State public var defined_type_name: String = "p"
+    @EnvironmentObject var config: configuration
+    @State private var description1: String = "Loading.."
+    @State private var paperURL: String = "Fetching..."
+    @State private var candidates: String = "Extracting Compounds"
+    func fetch(articleURL: String) {
+        //let url = URL(string: "\(articleURL)")!
+        var request = URLRequest(url: URL(string: articleURL)!)
+
+        request.httpMethod = "GET"
+        print(request)
+               // 2.
+               URLSession.shared.dataTask(with: request) {(data, response, error) in
+                   do {
+                       if let todoData = data {
+                           // 3.
+                        //let decodedData = try JSONDecoder().decode([Paper].self, from: todoData)
+                        let decodedData = try JSON(data: todoData)
+                           DispatchQueue.main.async {
+                            self.description1 = decodedData["description"].string!
+                            self.paperURL = decodedData["figshare_url"].string!
+                            print("Finding Answer")
+                            self.candidates = String(BERT().findAnswer(for: configuration().question, in: self.description1))
+                            //return decodedData
+                            
+                           }
+                       } else {
+                           print("No data")
+                       }
+                   } catch {
+                       print("Error")
+                   }
+               }.resume()
+        
+    }
+    let article: Todo
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(article.title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .contextMenu {
+                Button(action: {
+                    UIPasteboard.general.string = self.article.title
+                }) {
+                    Text("Copy")
+                    Image(systemName: "copy")
+                        }}
+                
+                Button("Paper URL") {
+                    UIApplication.shared.open(URL(string: self.paperURL)!)
+                }
+                Text("Possible Candidates")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                Text(self.candidates)
+                Text("Abstract")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                ScrollView(.vertical){
+                    VStack{
+                        Text(description1)
+                        //Text(description1.withoutHtml)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationBarTitle(Text(String(self.article.defined_type_name))
+            .font(.largeTitle)
+            .fontWeight(.black), displayMode: .inline)
+        }.onAppear {
+            self.fetch(articleURL: self.article.url)
+        }
+    }
+}
+
+#if DEBUG
+struct DetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedView(
+            article: Todo(
+                id: 0,
+                title: "Sample Paper Name",
+                published_date: "Excatly",
+                defined_type_name: "Lmao",
+                url: "https://google.com"
+                )
+        )
+    }
+}
+#endif
+
